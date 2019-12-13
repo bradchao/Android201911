4ca7d3c3bc56035d196845c33c1b01d88f5b24ea
brad <brad@brad.tw>
2019-12-13 19:21:32 +0800

    2

--- app/src/main/java/tw/org/iii/android201911/MainActivity.java
+++ app/src/main/java/tw/org/iii/android201911/MainActivity.java
@@ -1,14 +1,29 @@
 package tw.org.iii.android201911;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private P1 p1;
+    private P2 p2;
+    private P3 p3;
+    private FragmentManager fmgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        p1 = new P1(); p2 = new P2(); p3 = new P3();
+
+        fmgr = getSupportFragmentManager();
+        FragmentTransaction transaction = fmgr.beginTransaction();
+        transaction.add(R.id.container, p1);
+        transaction.commit();
+
+
     }
 }
