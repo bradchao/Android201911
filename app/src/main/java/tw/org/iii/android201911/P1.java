6db0c9cfcaa52387f9ea18cc6366e8e6b43c4588
brad <brad@brad.tw>
2019-12-13 19:53:47 +0800

    6

--- app/src/main/java/tw/org/iii/android201911/P1.java
+++ app/src/main/java/tw/org/iii/android201911/P1.java
@@ -22,22 +22,23 @@ public class P1 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        mainView = inflater.inflate(R.layout.fragment_p1, container, false);
-        tv1 = mainView.findViewById(R.id.P1TV1);
-        View p1b1 = mainView.findViewById(R.id.p1b1);
-        p1b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                clickB1();
-            }
-        });
+        if (mainView == null) {
+            mainView = inflater.inflate(R.layout.fragment_p1, container, false);
+            tv1 = mainView.findViewById(R.id.P1TV1);
+            View p1b1 = mainView.findViewById(R.id.p1b1);
+            p1b1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    clickB1();
+                }
+            });
+        }
 
 
         return mainView;
     }
 
     private void clickB1(){
-        Log.v("brad", "OK2");
         tv1.setText("" + (int)(Math.random()*49+1));
 
     }
